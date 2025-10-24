ce1603e623a9199369ea4c2dd6eecdcee9817dc6
ClOhmYee <cromi3423@gmail.com>
2025-10-24 15:25:20 +0900

    feat(try): add function to get input try count
    Signed-off-by: ClOhmYee <cromi3423@gmail.com>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         List<String> carList = getCarName();
+        int tries = getTries();
 
         // 추후 구현 예정
     }
@@ -21,8 +22,27 @@ public class Application {
             throw new IllegalArgumentException("오류: 자동차 이름을 최소 한 개 이상 입력해야 합니다.");
 
         return Arrays.stream(inputCarName.split(","))
-                                            .map(String::trim)
-                                            .filter(name -> !name.isEmpty())
-                                            .collect(Collectors.toList());
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    public static int getTries() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputTries = Console.readLine();
+        int numTries;
+
+        try {
+            numTries = Integer.parseInt(inputTries);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("오류: 올바르지 않은 시도 횟수입니다. ", e);
+        }
+
+        if (numTries < 0) {
+            throw new IllegalArgumentException("오류: 음수의 시도 횟수를 가질 수 없습니다.");
+        }
+
+        return numTries;
     }
 }
