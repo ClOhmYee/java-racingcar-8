2054547622d7780f62c51bf70df2723d00ac3376
ClOhmYee <cromi3423@gmail.com>
2025-10-24 15:08:10 +0900

    feat(car): add function to split input car names into a list
    Signed-off-by: ClOhmYee <cromi3423@gmail.com>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carList = getCarName();
+
+        // 추후 구현 예정
+    }
+
+
+    public static List<String> getCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarName = Console.readLine();
+
+        if (inputCarName == null || inputCarName.isBlank())
+            throw new IllegalArgumentException("오류: 자동차 이름을 최소 한 개 이상 입력해야 합니다.");
+
+        return Arrays.stream(inputCarName.split(","))
+                                            .map(String::trim)
+                                            .filter(name -> !name.isEmpty())
+                                            .collect(Collectors.toList());
     }
 }
